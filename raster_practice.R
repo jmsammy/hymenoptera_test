118a40572d176dc9ba546a16010a3051d8ca164a
Joshua Sammy <joshua.sammy@alumni.york.ac.uk>
2021-03-19 15:08:46 +0000

    Streamlining - commenting out code that is not being used.

--- raster_practice.R
+++ raster_practice.R
@@ -17,37 +17,39 @@ dictionary <- function(df, dict, old_col, new_col, dict_refcol, dict_defcol){
   return(df)
 }
 
-gb2019 <- raster("../gb2019lcm25m.tif")
-gb2019
-gb2019b1 <- raster("../gb2019lcm25m.tif", band=1)
-gb2019b2 <- raster("../gb2019lcm25m.tif", band=2)
-gb2019b3 <- raster("../gb2019lcm25m.tif", band=3)
-plot(gb2019b1)
-plot(gb2019b2)
-plot(gb2019b3)
-res(gb2019b1)
-gb20191k <- aggregate(gb2019b1, fact = 4, fun = modal)
-gb2019prop <- aggregate(gb2019b1, fact=4, fun = rs_prop_2)
-plot(gb2019prop)
-plot(gb20191k)
-area(gb20191k)
-gb2019_ha <- stack(gb20191k, gb2019prop)
+#Commenting out this code because I can use the tif I made for this instead.
 
-writeRaster(x = gb2019_ha,
-            
-            # where your file will go - update with your file path!
-            
-            filename="gb2019_ha.tif", 	
-            format = "GTiff") 
+# gb2019 <- raster("../gb2019lcm25m.tif")
+# gb2019
+# gb2019b1 <- raster("../gb2019lcm25m.tif", band=1)
+# gb2019b2 <- raster("../gb2019lcm25m.tif", band=2)
+# gb2019b3 <- raster("../gb2019lcm25m.tif", band=3)
+# plot(gb2019b1)
+# plot(gb2019b2)
+# plot(gb2019b3)
+# res(gb2019b1)
+# gb20191k <- aggregate(gb2019b1, fact = 4, fun = modal)
+# gb2019prop <- aggregate(gb2019b1, fact=4, fun = rs_prop_2)
+# plot(gb2019prop)
+# plot(gb20191k)
+# area(gb20191k)
+# gb2019_ha <- stack(gb20191k, gb2019prop)
+# 
+# writeRaster(x = gb2019_ha,
+#             
+#             # where your file will go - update with your file path!
+#             
+#             filename="gb2019_ha.tif", 	
+#             format = "GTiff") 
+
 #I can now read in the tif without having to make it afresh every time.
 
 gb20191k <- raster("gb2019_ha.tif", band=1) 
 gb2019prop <- raster("gb2019_ha.tif", band=2) 
+gb2019_ha <- stack(gb20191k, gb2019prop)
 
-hist(gb2019prop)
-sr2019 <- "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs"
-
-#####
+#############
+#Anthromes Bit.
 world2015 <- raster("../anthromes2015AD.asc")
 plot(world2015)
 e <- extent(-11.733, 2.285, 49.582, 61.186)
@@ -63,6 +65,7 @@ anthgb20151k <- disaggregate(proj_anthgb2015, fact=c(52, 92), fun=modal)
 area(anthgb20151k)
 plot(anthgb20151k)
 
+#############
 #Read in all hymenoptera data
 hymenoptera <- read_csv("../records_processed.csv")
 hymenoptera <- Filter(function(x)!all(is.na(x)), hymenoptera)
@@ -82,6 +85,8 @@ event_split <- str_split_fixed(hymenoptera$event_date, "-", 3)
 colnames(event_split) <- event_colnames
 
 hymenoptera <- cbind(hymenoptera, event_split)
+rm(event_split)
+rm(event_colnames)
 
 hymenoptera$year2020 <- ifelse(hymenoptera$event_year == 2020, TRUE, FALSE)
 
@@ -106,6 +111,8 @@ for(i in (1:length(hymenoptera$grid_reference))){
   hymenoptera$northing[i] <- foo$northing
 }
 rm(foo)
+rm(df)
+rm(i)
 
 hymenoptera$easting <- floor(hymenoptera$easting/100)*100 # ensure x precision is exactly 100m, referenced to left
 hymenoptera$northing <- floor(hymenoptera$northing/100)*100 # ensure y precision is exactly 100m, referenced to bottom
@@ -114,66 +121,66 @@ hymenoptera$event_date <- as.Date(hymenoptera$event_date)
 str(hymenoptera$event_date)
 unique_Months <- format(hymenoptera$event_date, "%B_%Y")
 
-hymenoptera_nona <- hymenoptera %>%
-  filter(!is.na(scientific_name_processed)) %>%
-  filter(!is.na(plan_no)) %>%
-  filter(!is.na(event_date)) %>%
-  filter(!is.na(event_year)) %>%
-  filter(!is.na(event_month))
-
-hymenoptera_subset  <- siteSelection(taxa = hymenoptera_nona$scientific_name_processed,
-                               site = hymenoptera_nona$plan_no,
-                               time_period = hymenoptera_nona$event_date,
-                               minL=2)
-
-system.time({
-  occ_out <- occDetModel(taxa = hymenoptera_nona$scientific_name_processed,
-                         site = hymenoptera_nona$plan_no,
-                         survey = hymenoptera_nona$event_date,
-                         write_results = FALSE,
-                         n_iterations = 200,
-                         burnin = 15,
-                         n_chains = 3,
-                         thinning = 3,
-                         seed = 123)
-})
-
-library(snowfall)
-
-# I have 12 logical processors on my PC so I set cpus to 11 (n-1)
-# when I initialise the cluster
-sfInit(parallel = TRUE, cpus = 11)
-
-# Export my data to the cluster
-sfExport('formattedOccData')
-
-# I create a function that takes a species name and runs my model
-occ_mod_function <- function(taxa_name){
-  
-  library(sparta)
-  
-  occ_out <- occDetModel(taxa = taxa_name,
-                         site = hymenoptera$plan_no,
-                         survey = unique_Months,
-                         write_results = FALSE,
-                         n_iterations = 200,
-                         burnin = 15,
-                         n_chains = 3,
-                         thinning = 3,
-                         seed = 123)
-} 
-
-# I then run this in parallel
-
-system.time({
-  taxa <- levels(as.factor(hymenoptera$scientific_name_processed))
-  para_out <- sfClusterApplyLB(taxa, occ_mod_function)
-})
-# Name each element of this output by the species
-for(i in  1:length(para_out)) names(para_out)[i] <- para_out[[i]]$SPP_NAM
-
-# Stop the cluster
-sfStop()
+# hymenoptera_nona <- hymenoptera %>%
+#   filter(!is.na(scientific_name_processed)) %>%
+#   filter(!is.na(plan_no)) %>%
+#   filter(!is.na(event_date)) %>%
+#   filter(!is.na(event_year)) %>%
+#   filter(!is.na(event_month))
+# 
+# hymenoptera_subset  <- siteSelection(taxa = hymenoptera_nona$scientific_name_processed,
+#                                site = hymenoptera_nona$plan_no,
+#                                time_period = hymenoptera_nona$event_date,
+#                                minL=2)
+# 
+# system.time({
+#   occ_out <- occDetModel(taxa = hymenoptera_nona$scientific_name_processed,
+#                          site = hymenoptera_nona$plan_no,
+#                          survey = hymenoptera_nona$event_date,
+#                          write_results = FALSE,
+#                          n_iterations = 200,
+#                          burnin = 15,
+#                          n_chains = 3,
+#                          thinning = 3,
+#                          seed = 123)
+# })
+# 
+# library(snowfall)
+# 
+# # I have 12 logical processors on my PC so I set cpus to 11 (n-1)
+# # when I initialise the cluster
+# sfInit(parallel = TRUE, cpus = 11)
+# 
+# # Export my data to the cluster
+# sfExport('formattedOccData')
+# 
+# # I create a function that takes a species name and runs my model
+# occ_mod_function <- function(taxa_name){
+#   
+#   library(sparta)
+#   
+#   occ_out <- occDetModel(taxa = taxa_name,
+#                          site = hymenoptera$plan_no,
+#                          survey = unique_Months,
+#                          write_results = FALSE,
+#                          n_iterations = 200,
+#                          burnin = 15,
+#                          n_chains = 3,
+#                          thinning = 3,
+#                          seed = 123)
+# } 
+# 
+# # I then run this in parallel
+# 
+# system.time({
+#   taxa <- levels(as.factor(hymenoptera$scientific_name_processed))
+#   para_out <- sfClusterApplyLB(taxa, occ_mod_function)
+# })
+# # Name each element of this output by the species
+# for(i in  1:length(para_out)) names(para_out)[i] <- para_out[[i]]$SPP_NAM
+# 
+# # Stop the cluster
+# sfStop()
 
 gplot(gb20191k) +
   geom_raster(aes(x = x, y = y, fill = value)) +
@@ -195,6 +202,17 @@ col <- cbind(hymenoptera$easting+50, hymenoptera$northing+50) # Add 50 so that t
 land_use <- raster::extract(gb20191k, col)
 hymenoptera$land_use <- land_use
 
+#extract land use proportion
+
+land_use_prop <- raster::extract(gb2019prop, col)
+hymenoptera$land_use_prop <- land_use_prop
+
+#remove temp vectors and data frames
+
+rm(col)
+rm(land_use)
+rm(land_use_prop)
+
 #Use dictionary function for land use data
 
 landuse_dict <- readxl::read_excel("landuse_dict.xlsx")
@@ -209,6 +227,11 @@ hymenoptera <- dictionary(df = hymenoptera,
 ggplot(hymenoptera, aes(x=land_use, fill=scientific_name_processed))+
   geom_histogram()+
   theme(legend.position = "none")
+
+ggplot(hymenoptera, aes(x=land_use_names, y=land_use_prop, fill=land_use_names))+
+  geom_violin()
+
+# There's a lot of saltwater that's around 0.6... should I remove this? Will think about it.
 
 #Use dictionary function for anthrome data
 col <- cbind(hymenoptera$easting, hymenoptera$northing)
